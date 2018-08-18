096422a7683f4a72e521ccd1583649f2c32d25b6
Martin Louie Dela Serna <mldelaserna@Martins-MacBook-Pro.local>
2018-08-18 11:29:10 +0800

    Fix hover js

--- WebContent/assets/js/main.js
+++ WebContent/assets/js/main.js
@@ -1,13 +1,29 @@
 function hideShowSidebarSubmenu() {
-	$('.sidebar__menu li').hover(function() {
-		$(this).find('.sidebar__submenu ul').addClass('open');
+
+	// $('*:not(.sidebar__menu li.open)').hover(function() {
+	// 	$('.sidebar__menu li.open').removeClass('open');
+	// })
+
+	// $('.sidebar__menu li').hover(function() {
+	// 	$(this).addClass('open');
+	// })
+
+	$('.sidebar__menu li').on({
+		mouseenter: function() {
+			$(this).addClass('open');
+		},
+		mouseleave: function() {
+			$(this).removeClass('open')
+		}
 	})
+}
+
+function modalControls() {
 
 	$('.modal__bg, .modal__close').click(function() {
 		$('.modal').fadeOut();
 		console.log('clicked');
 	});
-
 }
 
 function dataTables() {
@@ -23,5 +39,6 @@ $(function() {
 
 	hideShowSidebarSubmenu();
 	dataTables();
+	modalControls();
 })
 
